4a07db9fd9a1961ae4dbcfa59857521833fa5224
tanhtt <flywtan@gmail.com>
2024-06-02 11:37:33 +0700

    init components checkbox,fieldset,combobox

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Example from "./test";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <Example></Example>
             </header>
         </div>
     );
---
+++ src/components/Checkbox.js
@@ -0,0 +1,17 @@
+import { Checkbox as CheckboxTailwind } from '@headlessui/react'
+import { CheckIcon } from '@heroicons/react/16/solid'
+import { useState } from 'react'
+
+export default function Checkbox() {
+  const [enabled, setEnabled] = useState(true)
+
+  return (
+    <CheckboxTailwind
+      checked={enabled}
+      onChange={setEnabled}
+      className="group size-6 rounded-md bg-white/10 p-1 ring-1 ring-white/15 ring-inset data-[checked]:bg-white"
+    >
+      <CheckIcon className="hidden size-4 fill-black group-data-[checked]:block" />
+    </CheckboxTailwind>
+  )
+}
