2859a3a39a80fb600baa986a41719883678aa0cf
Kiet0903 <htk09032003@gmail.com>
2024-06-02 12:59:05 +0700

    #4 -  add new component inputForm, ListBox, RadioGroup by Kietnehihi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import RadioGroup from "./components/RadioFroup";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
+                <RadioGroup/>
             </header>
         </div>
     );
