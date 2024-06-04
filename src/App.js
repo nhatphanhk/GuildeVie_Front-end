babf873df9e31c207bee2771a73dd266c65aea3e
Pickky03 <vhai02092003@gmail.com>
2024-06-04 13:36:28 +0700

    #6 - Component_Tabs_Transition_Button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import Button from "./components/Button";
+import Tabs from "./components/Tabs";
+import Transition from "./components/Transition";
 function App() {
     return (
         <div className="App">
             <header className="App-header"></header>
+            <Button/>
+            <Tabs/>
+            <Transition/>
         </div>
     );
 }
