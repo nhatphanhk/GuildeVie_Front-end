702a2c75bb8f260225a417f414e107efa059942d
nhatphanhk <114910072+nhatphanhk@users.noreply.github.com>
2024-06-04 23:18:19 +0700

    add new folder

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import FieldsetTailwind from "./components/Fieldset";
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <FieldsetTailwind></FieldsetTailwind>
+            </header>
+        </div>
+    );
+}
+
+export default App;
